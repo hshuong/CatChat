ec7e0c01eefe22ddefb35e151eb245485828da02
hshuong <hshuong7978@yahoo.com>
2023-09-20 22:14:07 +0700

    aaaa

--- app/src/main/java/com/hfad/catchat/MainActivity.kt
+++ app/src/main/java/com/hfad/catchat/MainActivity.kt
@@ -10,5 +10,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
         val toolbar = findViewById<MaterialToolbar>(R.id.toolbar)
         setSupportActionBar(toolbar)
+        // test here git
     }
 }
